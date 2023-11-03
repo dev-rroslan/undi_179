7e1d50ec9ccca38d53c4e1c7b2bd10fb148a72b3
dev-rroslan <user.email>
2023-11-04 03:40:57 +0800

    bumpup 1.7.10

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Undi.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.9"},
+      {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
